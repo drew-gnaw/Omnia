a5680542e767f1df342174d641021cc1b81cd583
Andrew Wang <115036225+drew-gnaw@users.noreply.github.com>
2025-02-11 23:57:35 -0800

    better nullcheck?

--- Assets/Scripts/Players/Mixin/UseInput.cs
+++ Assets/Scripts/Players/Mixin/UseInput.cs
@@ -38,7 +38,7 @@ namespace Players.Mixin {
         private float rlt;
 
         public void Update() {
-            var fire = !InventoryManager.Instance.IsInventoryOpen && Input.GetButtonDown(KeyMap[KeysEnum.Fire1]);
+            var fire = !(InventoryManager.Instance?.IsInventoryOpen ?? false) && Input.GetButtonDown(KeyMap[KeysEnum.Fire1]);
             var jump = Input.GetButtonDown(KeyMap[KeysEnum.Jump]);
             var held = Input.GetButton(KeyMap[KeysEnum.Jump]);
             var skill = Input.GetButtonDown(KeyMap[KeysEnum.Fire2]);
