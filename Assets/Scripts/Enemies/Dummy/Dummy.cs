2eae386086de60da9ff1f689597a72d2a2e54b04
r-k03 <rahul.kamath2512@gmail.com>
2025-02-19 18:12:51 -0800

    interactable world objects
    created a prefab that makes world objects interactable

--- Assets/Scripts/Enemies/Dummy/Dummy.cs
+++ Assets/Scripts/Enemies/Dummy/Dummy.cs
@@ -1,10 +1,15 @@
 using UnityEngine;
 
 namespace Enemies.Dummy {
-    public class Dummy : Enemy {
+    public class Dummy : Enemy, IInteractable {
+        [SerializeField] private DialogueWrapper dummyDialogue;
         public override void Hurt(float damage) {
             base.Hurt(damage);
             Debug.Log(this + " took damage " + damage);
+        }
+
+        public void Interact() {
+            StartCoroutine(DialogueManager.Instance.StartDialogue(dummyDialogue.Dialogue));
         }
     }
 }
---
+++ Assets/Scripts/Utils/Interactable.cs
@@ -0,0 +1,48 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    private bool inRange = false;
+    private IInteractable mainScript;
+    [SerializeField] private SpriteRenderer buttonIcon;
+
+    void Start() {
+        buttonIcon.enabled = false;
+        mainScript = GetComponentInParent<IInteractable>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.CompareTag("Player")) {
+            inRange = true;
+            buttonIcon.enabled = true;
+            //Debug.Log("can now interact");
+            //Debug.Log("null check:" + mainScript == null);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) {
+        if (collision.gameObject.CompareTag("Player")) {
+            inRange = false;
+            buttonIcon.enabled = false;
+            //Debug.Log("can no longer interact");
+        }
+    }
+
+    public bool GetPlayerRange() {
+        return inRange;
+    }
+
+    void Update() {
+        if (mainScript == null) return; 
+        if (inRange && Input.GetKeyDown(KeyCode.E)) {
+            mainScript.Interact();
+        }
+    }
+
+    void OnDestroy() {
+        mainScript = null;
+        buttonIcon = null;
+    }
+}
