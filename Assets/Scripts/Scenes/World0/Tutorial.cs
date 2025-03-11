3a7f05eef333d4a6b7640bac8b767a53326352e4
Andrew Wang <115036225+drew-gnaw@users.noreply.github.com>
2025-03-11 12:04:45 -0700

    dinky import + dummy work

--- Assets/Scripts/Scenes/World0/Tutorial.cs
+++ Assets/Scripts/Scenes/World0/Tutorial.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using Enemies.Dummy;
 using UI;
 using UnityEngine;
 using Utils;
@@ -8,14 +9,28 @@ namespace Scenes {
     public class Tutorial : MonoBehaviour {
         [SerializeField] private FadeScreenHandler fadeScreen;
 
-        [SerializeField] private GameObject dummy1;
-        [SerializeField] private GameObject dummy2;
-        [SerializeField] private GameObject dummy3;
+        [SerializeField] private GameObject dummy1Obj;
+        [SerializeField] private GameObject dummy2Obj;
+        [SerializeField] private GameObject dummy3Obj;
 
-        // DIALOGUE WRAPPERS
         [SerializeField] private DialogueWrapper beginDialogue;
+        [SerializeField] private DialogueWrapper thirdDummyDialogue;
 
-        public void Start() {
+        private Dummy dummy1;
+        private Dummy dummy2;
+        private Dummy dummy3;
+
+        private int dummiesHit = 0;
+        private bool thirdDialogueTriggered = false;
+
+        private void Start() {
+            dummy1 = dummy1Obj.GetComponent<Dummy>();
+            dummy2 = dummy2Obj.GetComponent<Dummy>();
+            dummy3 = dummy3Obj.GetComponent<Dummy>();
+
+            dummy1.OnHurt += HandleDummy1Hurt;
+            dummy2.OnHurt += HandleDummy2Hurt;
+
             StartCoroutine(BeginSequence());
         }
 
@@ -23,8 +38,33 @@ namespace Scenes {
             yield return StartCoroutine(fadeScreen.FadeInLightScreen(1f));
             yield return StartCoroutine(DialogueManager.Instance.StartDialogue(beginDialogue.Dialogue));
 
-            HighlightManager.Instance.HighlightGameObject(dummy1);
-            HighlightManager.Instance.HighlightGameObject(dummy2);
+            HighlightManager.Instance.HighlightGameObject(dummy1Obj);
+            HighlightManager.Instance.HighlightGameObject(dummy2Obj);
+        }
+
+        private void HandleDummy1Hurt() {
+            dummy1.OnHurt -= HandleDummy1Hurt;
+            HighlightManager.Instance.UnhighlightGameObject(dummy1Obj);
+            dummiesHit++;
+            if (dummiesHit >= 2 && !thirdDialogueTriggered) {
+                thirdDialogueTriggered = true;
+                StartCoroutine(ThirdDummySequence());
+            }
+        }
+
+        private void HandleDummy2Hurt() {
+            dummy1.OnHurt -= HandleDummy2Hurt;
+            HighlightManager.Instance.UnhighlightGameObject(dummy2Obj);
+            dummiesHit++;
+            if (dummiesHit >= 2 && !thirdDialogueTriggered) {
+                thirdDialogueTriggered = true;
+                StartCoroutine(ThirdDummySequence());
+            }
+        }
+
+        private IEnumerator ThirdDummySequence() {
+            yield return new WaitForSeconds(0.5f);
+            yield return StartCoroutine(DialogueManager.Instance.StartDialogue(thirdDummyDialogue.Dialogue));
         }
     }
 }
