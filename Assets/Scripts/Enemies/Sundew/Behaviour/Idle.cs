7d8f08217cea0d21c08da48f4827f5ccd649f5f2
Arthur Guo <arthur.guo.canada@gmail.com>
2025-03-20 14:10:27 -0700

    rework sundew mechanics

--- Assets/Scripts/Enemies/Sundew/Behaviour/Idle.cs
+++ Assets/Scripts/Enemies/Sundew/Behaviour/Idle.cs
@@ -1,35 +1,28 @@
+using UnityEngine;
+
 namespace Enemies.Sundew.Behaviour {
     public class Idle : IBehaviour {
         private readonly Sundew self;
-        private int layer;
+        private float t;
 
-        private Idle(Sundew self) {
+        public Idle(Sundew self) {
             this.self = self;
         }
 
         public void OnEnter() {
-            layer = self.gameObject.layer;
-
-            self.SetLayer(self.bg);
+            t = self.reload + self.lag;
         }
 
         public void OnExit() {
-            self.gameObject.layer = layer;
         }
 
         public void OnTick() {
+            if (t != 0) return;
+            self.UseBehaviour(new Attack(self));
         }
 
         public void OnUpdate() {
-            self.UseBehaviour(WindUp.If(self));
-        }
-
-        public static IBehaviour AsDefaultOf(Sundew it) {
-            return new Idle(it);
-        }
-
-        public static IBehaviour If(Sundew it) {
-            return !it.detected ? new Idle(it) : null;
+            t = Mathf.Max(0, t - Time.deltaTime);
         }
     }
 }
