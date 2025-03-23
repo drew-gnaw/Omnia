34f437e3d0bf09a21125c3ab439afa4c7f71fe3b
anjx123 <114792511+anjx123@users.noreply.github.com>
2025-03-23 16:33:40 -0700

    Colour and Symbol Reversion

--- Assets/Scripts/Puzzles/ISignal.cs
+++ Assets/Scripts/Puzzles/ISignal.cs
@@ -17,27 +17,27 @@ namespace Puzzle {
         public static SignalColor Parse(Type type) => ParseFromType(type);
     }
     public class Red : SignalColor {
-        public override Color Color => new Color(218f / 255f, 66f / 255f, 66f / 255f);
+        public override Color Color => Color.red;
 
         public override Sprite GetSymbol(PuzzleAssets assets) => assets.xSymbol;
     }
     public class Green : SignalColor {
-        public override Color Color => new Color(93f / 255f, 166f / 255f, 69f / 255f);
+        public override Color Color => Color.green;
 
         public override Sprite GetSymbol(PuzzleAssets assets) => assets.triangleSymbol;
     }
     public class Blue : SignalColor {
-        public override Color Color => new Color(58f / 255f, 139f / 255f, 144f / 255f);
+        public override Color Color => Color.blue;
 
         public override Sprite GetSymbol(PuzzleAssets assets) => assets.squareSymbol;
     }
     public class Yellow : SignalColor {
-        public override Color Color => new Color(237f / 255f, 193f / 255f, 70f / 255f);
+        public override Color Color => Color.yellow;
 
         public override Sprite GetSymbol(PuzzleAssets assets) => assets.xSymbol;
     }
     public class Black : SignalColor {
-        public override Color Color => new Color(.4f,.4f,.4f);
+        public override Color Color => new Color(.5f,.5f,.5f);
         public override Sprite GetSymbol(PuzzleAssets assets) => assets.xSymbol;
     }
 }
--- Assets/Scripts/Puzzles/PuzzleSymbol.cs
+++ Assets/Scripts/Puzzles/PuzzleSymbol.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 namespace Puzzle {
     public class PuzzleSymbol : MonoBehaviour {
         [SerializeField] private SpriteRenderer symbolSpriteRenderer;
+        [SerializeField] private SpriteRenderer mainSpriteRenderer;
         public void SetSymbol(Sprite sprite) => symbolSpriteRenderer.sprite = sprite;
+        public void SetColor(Color c) => mainSpriteRenderer.color = c;
     }
 }
