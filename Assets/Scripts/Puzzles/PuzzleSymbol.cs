add3914b56922839b779a9cdce8260f57d3307b7
anjx123 <114792511+anjx123@users.noreply.github.com>
2025-03-23 16:19:16 -0700

    Crab Bossfight Idea Finished

--- Assets/Scripts/Puzzles/PuzzleSymbol.cs
+++ Assets/Scripts/Puzzles/PuzzleSymbol.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 namespace Puzzle {
-    public class ConveyerEndPiece : MonoBehaviour {
+    public class PuzzleSymbol : MonoBehaviour {
         [SerializeField] private SpriteRenderer symbolSpriteRenderer;
         public void SetSymbol(Sprite sprite) => symbolSpriteRenderer.sprite = sprite;
     }
