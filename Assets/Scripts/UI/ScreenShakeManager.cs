557d96a029087de995933a8d204d87dd20f6ef19
Andrew Wang <115036225+drew-gnaw@users.noreply.github.com>
2025-03-24 14:39:46 -0700

    fix scene manager nullref?

--- Assets/Scripts/UI/ScreenShakeManager.cs
+++ Assets/Scripts/UI/ScreenShakeManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using Cinemachine;
 using Players.Buff;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using Utils;
 
 public class ScreenShakeManager : PersistentSingleton<ScreenShakeManager> {
@@ -9,22 +10,23 @@ public class ScreenShakeManager : PersistentSingleton<ScreenShakeManager> {
     private CinemachineBasicMultiChannelPerlin perlinNoise;
 
     protected override void OnAwake() {
-        virtualCamera = FindObjectOfType<CinemachineVirtualCamera>();
-        if (!virtualCamera) {
-            Debug.Log("couldn't find virtual camera");
-        }
-        perlinNoise = virtualCamera.GetComponentInChildren<CinemachineBasicMultiChannelPerlin>();
+        UpdateCameraReference();
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
 
-        if (perlinNoise == null) {
-            Debug.LogError("CinemachineBasicMultiChannelPerlin is missing from the camera!");
-        }
+    private void OnDisable() {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode) {
+        UpdateCameraReference();
     }
 
     public void Shake(float intensity = 1.0f, float duration = 0.5f) {
         if (perlinNoise != null) {
             StartCoroutine(ShakeCoroutine(intensity, duration));
         } else {
-          //  Debug.LogError("Perlin noise module is missing!");
+            Debug.LogError("Perlin noise module is missing!");
         }
     }
 
@@ -52,5 +54,20 @@ public class ScreenShakeManager : PersistentSingleton<ScreenShakeManager> {
         // Reset to the original values
         perlinNoise.m_AmplitudeGain = originalAmplitude;
         perlinNoise.m_FrequencyGain = originalFrequency;
+    }
+
+    private void UpdateCameraReference() {
+        virtualCamera = FindObjectOfType<CinemachineVirtualCamera>();
+
+        if (!virtualCamera) {
+            Debug.LogWarning("Couldn't find virtual camera in scene!");
+            return;
+        }
+
+        perlinNoise = virtualCamera.GetComponentInChildren<CinemachineBasicMultiChannelPerlin>();
+
+        if (!perlinNoise) {
+            Debug.LogError("CinemachineBasicMultiChannelPerlin component is missing from the camera!");
+        }
     }
 }
