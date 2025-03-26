7703610ca5b1de07abf066f0dfa0723308c7cb44
Andrew Wang <115036225+drew-gnaw@users.noreply.github.com>
2025-03-25 22:21:01 -0700

    level selecting

--- Assets/Scripts/Scenes/LevelSelect.cs
+++ Assets/Scripts/Scenes/LevelSelect.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using Background;
 using Initializers;
 using TMPro;
 using UnityEngine;
@@ -10,37 +11,46 @@ using Utils;
 namespace Scenes {
     public class LevelSelect : MonoBehaviour
     {
-        public Button[] buttons;
-        public GameObject levelButtons;
-        [SerializeField] protected FadeScreenHandler fadeScreen;
+        [SerializeField] private GameObject dustParent;
 
-        protected virtual void Awake()
-        {
-            ButtonArray();
-            fadeScreen.SetDarkScreen();
-            StartCoroutine(fadeScreen.FadeInLightScreen(1f));
-        }
+        private Image[] dustImages;
 
-        public void OpenScene(string s)
-        {
-            StartCoroutine(FadeLevelIn(s));
-        }
+        private void Start() {
+            dustImages = dustParent.GetComponentsInChildren<Image>();
 
-        IEnumerator FadeLevelIn(string levelName)
-        {
-            yield return StartCoroutine(fadeScreen.FadeInDarkScreen(0.8f));
-            SceneManager.LoadScene(levelName);
-        }
-
-        void ButtonArray()
-        {
-            int children = levelButtons.transform.childCount;
-            buttons = new Button[children];
-            for (int i = 0; i < children; i++)
-            {
-                buttons[i] = levelButtons.transform.GetChild(i).gameObject.GetComponent<Button>();
+            foreach (var img in dustImages) {
+                img.gameObject.AddComponent<FloatingImage>();
             }
         }
+
+
+        public void GoToWorld(int worldNumber) {
+            switch (worldNumber) {
+                case 0:
+                    SceneInitializer.LoadScene("2_Opening");
+                    break;
+                case 1:
+                    SceneInitializer.LoadScene("W-1-2");
+                    break;
+                case 2:
+                    SceneInitializer.LoadScene("W-2-1");
+                    break;
+                case 3:
+                    SceneInitializer.LoadScene("W-3-1");
+                    break;
+                case 4:
+                    SceneInitializer.LoadScene("W-4-1");
+                    break;
+                case 5:
+                    SceneInitializer.LoadScene("W-B-B");
+                    break;
+                default:
+                    Debug.Log("Invalid world number");
+                    SceneInitializer.LoadScene("1_Title");
+                    break;
+            }
+        }
+
     }
 
 
