00743ef0ae726bf676c49a4d36429dfbfd184b4e
Andrew Wang <115036225+drew-gnaw@users.noreply.github.com>
2025-03-25 23:28:50 -0700

    pause menu and shit

--- Assets/Scripts/LevelSelect/PauseMenu.cs
+++ Assets/Scripts/LevelSelect/PauseMenu.cs
@@ -7,8 +7,7 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using Utils;
 
-public class PauseMenu : PersistentSingleton<PauseMenu>
-{
+public class PauseMenu : PersistentSingleton<PauseMenu> {
     public static bool IsPaused = false;
     public static bool ScreenShake = true;
     [SerializeField] private GameObject pauseMenuPanel;
@@ -20,29 +19,26 @@ public class PauseMenu : PersistentSingleton<PauseMenu>
 
     [SerializeField] private TextMeshProUGUI historyText;
 
-    [SerializeField]
-    private Slider musicSlider;
+    [SerializeField] private Slider musicSlider;
 
 
     public delegate void PauseMenuEventHandler();
-    public static event PauseMenuEventHandler onPauseMenuActivate;   // for more complex functions that cannot use isPaused
+
+    public static event PauseMenuEventHandler onPauseMenuActivate; // for more complex functions that cannot use isPaused
     public static event PauseMenuEventHandler onPauseMenuDeactivate;
 
 
     bool isDialogueHistoryShowing = false;
-    public bool DialogueHistoryState
-    {
+
+    public bool DialogueHistoryState {
         get { return isDialogueHistoryShowing; }
-        set
-        {
-            if (value == true)
-            {
+        set {
+            if (value == true) {
                 ShowHistory();
-            }
-            else
-            {
+            } else {
                 HideHistory();
             }
+
             isDialogueHistoryShowing = value;
         }
     }
@@ -51,112 +47,96 @@ public class PauseMenu : PersistentSingleton<PauseMenu>
         return;
     }
 
-    void ShowHistory()
-    {
+    void ShowHistory() {
         RenderHistory();
         history.SetActive(true);
         buttonparent.SetActive(false);
     }
 
-    void HideHistory()
-    {
+    void HideHistory() {
         history.SetActive(false);
         buttonparent.SetActive(true);
     }
 
-    void RenderHistory()
-    {
+    void RenderHistory() {
         List<DialogueText> list = DialogueManager.Instance?.GetHistory();
         historyText.text = "";
-        foreach (DialogueText dialogue in list)
-        {
+        foreach (DialogueText dialogue in list) {
             historyText.text += "<b>" + dialogue.SpeakerName + "</b>" + "\n" + dialogue.BodyText + "\n\n";
         }
     }
 
-    public void SetHistoryState(bool state)
-    {
+    public void SetHistoryState(bool state) {
         DialogueHistoryState = state;
     }
 
 
-    private void Start()
-    {
+    private void Start() {
         musicSlider.value = 0.7f;
         MusicVolume();
     }
+
     // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Tab))
-        {
-            if (IsPaused)
-            {
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Tab)) {
+            if (IsPaused) {
                 ResumeScene();
-            } else
-            {
+            } else {
                 PauseScene();
             }
         }
     }
 
 
-
-
-    public void ResumeScene()
-    {
+    public void ResumeScene() {
         pauseMenuPanel.SetActive(false);
         onPauseMenuDeactivate?.Invoke();
         Time.timeScale = 1f;
         IsPaused = false;
     }
 
-    public void PauseScene()
-    {
+    public void PauseScene() {
         pauseMenuPanel.SetActive(true);
         onPauseMenuActivate?.Invoke();
         Time.timeScale = 0f;
         IsPaused = true;
 
-        if (isDialogueHistoryShowing)
-        {
+        if (isDialogueHistoryShowing) {
             RenderHistory();
         }
     }
 
-    public void LevelSelectScene()
-    {
+    public void LevelSelectScene() {
         ResumeScene();
-        SceneInitializer.LoadScene(SceneManager.GetActiveScene().name); //change when level select added
+        SceneInitializer.LoadScene("LevelSelect");
     }
 
-    public void ResetScene()
-    {
+    public void MainMenuScene() {
+        ResumeScene();
+        SceneInitializer.LoadScene("1_Title");
+    }
+
+    public void ResetScene() {
         ResumeScene();
         SceneInitializer.LoadScene(SceneManager.GetActiveScene().name);
     }
 
-    public void SkipScene()
-    {
+    public void SkipScene() {
         ResumeScene();
         LevelManager.Instance.NextLevel();
     }
 
-    public void ToggleMusic()
-    {
-       AudioManager.Instance.ToggleAudio();
+    public void ToggleMusic() {
+        AudioManager.Instance.ToggleAudio();
     }
 
-    public void MusicVolume()
-    {
+    public void MusicVolume() {
         AudioManager.Instance.SetBGMVolume(musicSlider.value);
         AudioManager.Instance.SetSFXVolume(musicSlider.value);
         AudioManager.Instance.SetAmbientVolume(musicSlider.value);
-
     }
 
-    public void ToggleScreenShake()
-    {
+    public void ToggleScreenShake() {
         PauseMenu.ScreenShake = !PauseMenu.ScreenShake;
     }
 }
