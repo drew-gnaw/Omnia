534d83f538695bd2f647b6cdcfb66776364fa099
Sammy Kuang <sammy.kuang04@gmail.com>
2025-03-27 23:46:36 -0700

    Additional harpoon pickup safety check

--- Assets/Scripts/Weapon/HarpoonGun/HarpoonSpear.cs
+++ Assets/Scripts/Weapon/HarpoonGun/HarpoonSpear.cs
@@ -99,7 +99,7 @@ public class HarpoonSpear : MonoBehaviour {
             float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
             transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);
 
-            if (Vector3.Distance(transform.position, player.Center) <= 0.1f) {
+            if (collectable && Vector3.Distance(transform.position, player.Center) <= 0.1f) {
                 HandlePlayerCollision();
             }
         }
