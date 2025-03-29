f062340f35fb5e32fe6fb8d2509ca2052e98bfa2
Andrew Wang <115036225+drew-gnaw@users.noreply.github.com>
2025-03-28 22:27:30 -0700

    import walk

--- Assets/Scripts/NPC/Dinky/Dinky.cs
+++ Assets/Scripts/NPC/Dinky/Dinky.cs
@@ -3,19 +3,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
-using Random = UnityEngine.Random;
 
 public class Dinky : MonoBehaviour, IInteractable
 {
     [SerializeField] private Interactable interactable;
     [SerializeField] private GameObject graphics;
-    [SerializeField] private string idleState;
-    [SerializeField] private string appearState;
-    [SerializeField] private string disappearState;
     [SerializeField] private Animator animator;
     [FormerlySerializedAs("tempDialogue")] [SerializeField] private DialogueWrapper interactDialogue;
-
-    // Not sure this is how it should be handled, could stay this simple if Dinky's interactions are totally linear
     [SerializeField] private List<Transform> locations;
 
     private static readonly int AppearTrigger = Animator.StringToHash("Appear");
@@ -24,73 +18,49 @@ public class Dinky : MonoBehaviour, IInteractable
 
     public static event Action OnInteract;
 
-    private bool animating = false;
-
-    public void Appear(Transform t) {
-        gameObject.transform.position = t.position;
-        if (!animator) {
+    private void Awake()
+    {
+        if (!animator && graphics)
+        {
             animator = graphics.GetComponent<Animator>();
         }
-        animator.SetTrigger(AppearTrigger);
+    }
+
+    public void Appear(Transform t)
+    {
+        if (!animator) return;
+
+        gameObject.transform.position = t.position;
         setVisible(true);
-        animating = true;
+        animator.SetTrigger(AppearTrigger);
     }
 
-    public void Disappear() {
+    public void Disappear()
+    {
+        if (!animator) return;
+
         interactable?.SetEnable(false);
-        animator.Play(disappearState);
-        animating = true;
+        animator.SetTrigger(DisappearTrigger);
     }
 
-    public void Interact() {
-        // This is a demo of how Dinky could interact
+    public void Interact()
+    {
         StartCoroutine(StartDialogue());
-        // I think a list of observers in DialogueManager is worth looking into if other classes are interested
-        // in listening to the end of the dialogue, otherwise this will probably be fine
-
-        //StartCoroutine(dinkyReappearElsewhere());
     }
 
-    private IEnumerator StartDialogue() {
+    private IEnumerator StartDialogue()
+    {
         yield return StartCoroutine(DialogueManager.Instance.StartDialogue(interactDialogue.Dialogue));
         OnInteract?.Invoke();
     }
 
-    // demo of dinky behaviour
-    private IEnumerator dinkyReappearElsewhere() {
-        yield return new WaitUntil(DialogueManager.Instance.IsInDialogue);
-        Disappear();
-        yield return new WaitUntil(() => !animating);
-        Appear(locations[Random.Range(0, locations.Count)]);
-    }
 
-    private void appeared() {
-        animating = false;
-        interactable?.SetEnable(true);
-        animator.Play(idleState);
-    }
-
-    private void disappeared() {
-        animating = false;
-        setVisible(false);
-    }
-
-    private void setVisible(bool visible) {
-        graphics.GetComponentInChildren<Renderer>().enabled = visible;
-    }
-
-    void Update()
+    private void setVisible(bool visible)
     {
-        AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
-
-        // This is a pretty jank way to check if animation has completed, but probably quickest and easiest
-        if (animating && stateInfo.normalizedTime >= 1.0f)
+        if (graphics)
         {
-            if (stateInfo.IsName(appearState)) {
-                appeared();
-            } else if (stateInfo.IsName(disappearState)) {
-                disappeared();
-            }
+            Renderer renderer = graphics.GetComponentInChildren<Renderer>();
+            if (renderer) renderer.enabled = visible;
         }
     }
 }
