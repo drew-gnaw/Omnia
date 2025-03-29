7202212a27af7ea8f55604cdc1715003009b4b62
Andrew Wang <115036225+drew-gnaw@users.noreply.github.com>
2025-03-28 18:58:36 -0700

    uncle walkin

--- Assets/Scripts/Scenes/World5/RuinedTown.cs
+++ Assets/Scripts/Scenes/World5/RuinedTown.cs
@@ -1,9 +1,26 @@
+using NPC;
 using UnityEngine;
 
 namespace Scenes.World5 {
+    [RequireComponent(typeof(BoxCollider2D))]
     public class RuinedTown : MonoBehaviour {
-        public void Start() {
 
+        [SerializeField] private LayerMask playerLayer;
+        [SerializeField] private Uncle uncle;
+
+        private bool uncleTriggered;
+
+        private void OnTriggerEnter2D(Collider2D other) {
+            if (((1 << other.gameObject.layer) & playerLayer) != 0) {
+                OnPlayerEnter();
+            }
+        }
+
+        private void OnPlayerEnter() {
+            if (uncleTriggered) return;
+            Debug.Log("Player entered the uncle's trigger area!");
+            uncleTriggered = true;
+            uncle.Walk(-5f, 3f);
         }
     }
 }
