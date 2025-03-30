db001a606a0af37398b3f27465c65562d04901b0
Andrew Wang <115036225+drew-gnaw@users.noreply.github.com>
2025-03-29 21:00:46 -0700

    epilogue start

---
+++ Assets/Scripts/Scenes/World5/Lab Epilogue.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using Players;
+using Players.Mixin;
+using UnityEngine;
+
+namespace Scenes.World5 {
+    public class Lab_Epilogue : MonoBehaviour {
+        [SerializeField] private GameObject dinky;
+        [SerializeField] private GameObject boss;
+        [SerializeField] private Animator bossAnimator;
+
+        [SerializeField] DialogueWrapper beginDialogue;
+        public void Start() {
+            StartCoroutine(BeginSequence());
+        }
+
+        private IEnumerator BeginSequence() {
+            yield return new WaitForSeconds(0.5f);
+
+            
+        }
+
+
+    }
+
+}
