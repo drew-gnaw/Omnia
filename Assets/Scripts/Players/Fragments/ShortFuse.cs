cc2edf4b5b3b770fd82db391781535c0ceb3d71d
Andrew Wang <115036225+drew-gnaw@users.noreply.github.com>
2025-04-02 19:59:50 -0700

    short fuse

---
+++ Assets/Scripts/Players/Fragments/ShortFuse.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using Enemies;
+using UnityEngine;
+
+namespace Players.Fragments {
+    public class ShortFuse : Fragment {
+        [SerializeField] internal GameObject explosion;
+        [SerializeField] internal float explosionRadius;
+        [SerializeField] internal float damage;
+        [SerializeField] internal float knockbackForce;
+        private float Explode(float incomingDamage) {
+            Instantiate(explosion, player.Center, Quaternion.identity);
+            AudioManager.Instance.PlaySFX(AudioTracks.FlyBoom);
+
+            Collider2D[] hits = Physics2D.OverlapCircleAll(player.Center, explosionRadius);
+
+            foreach (var hit in hits) {
+                Enemy enemy = hit.GetComponent<Enemy>();
+                if (enemy != null) {
+                    enemy.Hurt(damage);
+
+                    Rigidbody2D enemyRb = enemy.GetComponent<Rigidbody2D>();
+                    if (enemyRb != null) {
+                        Vector2 knockbackDirection = (enemy.transform.position - player.Center).normalized;
+                        enemyRb.AddForce(knockbackDirection * knockbackForce, ForceMode2D.Impulse);
+                    }
+                }
+            }
+
+            return incomingDamage;
+        }
+
+        public override void ApplyBuff() {
+            OnDamageTaken.AddLast(Explode);
+        }
+
+        public override void RevokeBuff() {
+            OnDamageTaken.Remove(Explode);
+        }
+    }
+}
