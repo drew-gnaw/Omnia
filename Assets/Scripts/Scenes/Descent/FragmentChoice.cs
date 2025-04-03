cfa5e887ff97b6f1f0cb078e4b4304b1a66ed6a0
Andrew Wang <115036225+drew-gnaw@users.noreply.github.com>
2025-04-02 23:52:40 -0700

    selection

--- Assets/Scripts/Scenes/Descent/FragmentChoice.cs
+++ Assets/Scripts/Scenes/Descent/FragmentChoice.cs
@@ -1,11 +1,16 @@
 using Players.Buff;
 using Players.Fragments;
+using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.UIElements;
+using Image = UnityEngine.UI.Image;
 
 namespace Scenes.Descent {
-    public class FragmentChoice : MonoBehaviour {
+    public class FragmentChoice : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler {
         [SerializeField] private Image fragmentImage;
+        [SerializeField] private TextMeshProUGUI title;
+        [SerializeField] private TextMeshProUGUI description;
         private Fragment fragment;
         private Color originalColor;
 
@@ -18,21 +23,23 @@ namespace Scenes.Descent {
                 fragmentImage = GetComponent<Image>();
             }
             originalColor = fragmentImage.color;
+            title.text = fragment.fragmentName;
+            description.text = fragment.description;
         }
 
-        public void OnPointerEnter() {
+        public void OnPointerEnter(PointerEventData eventData) {
             if (fragmentImage != null) {
                 fragmentImage.color = new Color(originalColor.r * 0.7f, originalColor.g * 0.7f, originalColor.b * 0.7f, originalColor.a); // Darken by 30%
             }
         }
 
-        public void OnPointerExit() {
+        public void OnPointerExit(PointerEventData eventData) {
             if (fragmentImage != null) {
                 fragmentImage.color = originalColor;
             }
         }
 
-        public void OnClick() {
+        public void OnPointerClick(PointerEventData eventData) {
             if (fragment != null) {
                 BuffManager.Instance.ApplyBuff(fragment);
                 LevelManager.Instance.NextLevel();
