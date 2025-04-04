0ac201f3c939b9338b16c32f8b39f0852b2a0cf0
Andrew Wang <115036225+drew-gnaw@users.noreply.github.com>
2025-04-04 13:58:42 -0700

    reroll

--- Assets/Scripts/Scenes/WarpedDepths/FragmentSelection.cs
+++ Assets/Scripts/Scenes/WarpedDepths/FragmentSelection.cs
@@ -30,8 +30,11 @@ namespace Scenes {
             DisablePersistentSingletons.DisableInventory();
             DisablePersistentSingletons.DisablePause();
 
-            FragmentChoice[] choices = FindObjectsOfType<FragmentChoice>();
+            RandomFragments();
+        }
 
+        private void RandomFragments() {
+            FragmentChoice[] choices = FindObjectsOfType<FragmentChoice>();
             List<Fragment> fragmentOptions = BuffManager.Instance.GetRandomizedFragments(choices.Length);
 
             int count = Mathf.Min(choices.Length, fragmentOptions.Count);
@@ -39,12 +42,20 @@ namespace Scenes {
             for (int i = 0; i < count; i++) {
                 choices[i].SetFragment(fragmentOptions[i]);
             }
-
         }
 
         public void StartLevel() {
             LevelManager.Instance.NextLevel();
         }
+
+        #if UNITY_EDITOR
+        private void Update() {
+            if (Input.GetKeyDown(KeyCode.Escape)) {
+                RandomFragments();
+            }
+        }
+
+        #endif
 
     }
 
