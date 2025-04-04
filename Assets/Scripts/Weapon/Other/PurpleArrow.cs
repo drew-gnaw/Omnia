f13e1a699714a53b8157e4a79f38dab0b2124339
Andrew Wang <115036225+drew-gnaw@users.noreply.github.com>
2025-04-04 16:36:17 -0700

    oih yeah yeah

--- Assets/Scripts/Weapon/Other/PurpleArrow.cs
+++ Assets/Scripts/Weapon/Other/PurpleArrow.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using Enemies;
 using Players;
@@ -21,6 +22,8 @@ public class PurpleArrow : MonoBehaviour {
     private Vector2 currentDirection;
     private Rigidbody2D rb;
     private float damage;
+
+    public static bool gainCritOnHit;
 
     [SerializeField] private ParticleSystem trailParticles;
     [SerializeField] private GameObject impactParticlesPrefab;
@@ -70,16 +73,26 @@ public class PurpleArrow : MonoBehaviour {
         Enemy enemy = other.GetComponent<Enemy>();
         if (enemy != null) {
             Player player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
-            enemy.Hurt(damage * player.damageMultiplier * Mathf.Clamp(player.critChance, 0.2f, 2f) * player.critMultiplier, crit: true);
+            enemy.Hurt(damage * player.damageMultiplier * Mathf.Clamp(player.critChance, 0.2f, 10f) * player.critMultiplier, crit: true);
 
             if (impactParticlesPrefab != null) {
                 GameObject burst = Instantiate(impactParticlesPrefab, transform.position, Quaternion.identity);
                 Destroy(burst, 1.5f);
             }
 
+            if (gainCritOnHit) {
+                StartCoroutine(GainCriticalChance(player));
+            }
+
             DetachAndCleanupParticles();
             Destroy(gameObject);
         }
+    }
+
+    private IEnumerator GainCriticalChance(Player player) {
+        player.critChance += 0.1f;
+        yield return new WaitForSeconds(4f);
+        player.critChance -= 0.1f;
     }
 
 
